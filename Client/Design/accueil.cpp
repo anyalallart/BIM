72f73c21c21abeaf4fefcbdd0403184a8d3983e5
Camille LANGER <camille.langer@student.junia.com>
2022-12-23 17:27:38 +0100

    read account

--- Client/Design/accueil.cpp
+++ Client/Design/accueil.cpp
@@ -12,15 +12,23 @@ TAcc::TAcc(const wxString& title, const wxPoint& pos, const wxSize& size,
 
     auto *txt = new wxStaticText(this, -1, _T("Voici vos comptes :"), wxPoint(80,20));
 
-    for (int i = 1; i < 4; i++) {
-        auto* button = new wxButton(this, FIRST_BUTTON_ID+i);
+
+    std::string request ="SELECT * FROM compte WHERE client='"+ std::to_string(wxGetApp().user.id)+"'";
+    std::vector<std::map<std::string, std::string>> result = wxGetApp().database.select(request);
+
+    std::string request2 ="SELECT * FROM type_compte";
+    std::vector<std::map<std::string, std::string>> result2 = wxGetApp().database.select(request2);
+
+
+    for (int i = 0; i < result.size(); i++) {
+        auto* button = new wxButton(this, FIRST_BUTTON_ID+i,result2[stoi(result[i]["type"])-1]["nom"]);
         button->Connect(
                 FIRST_BUTTON_ID+i,
                 wxEVT_BUTTON,
                 wxCommandEventHandler(TAcc::OnButtonClick),
                 nullptr,
                 this);
-        button->SetPosition(wxPoint(100,50*i));
+        button->SetPosition(wxPoint(100,50*(i+1)));
 
         //auto *vir = new wxStaticText(this, -1, _T("Virements"), wxPoint(300,20));
 
@@ -33,6 +41,8 @@ void TAcc::OnButtonClick(wxCommandEvent& evt) {
     Close();
     TInf *info = new TInf("Informations compte");
     info->Show(true);
+
+
 }
 
 void TAcc::Vir(wxCommandEvent &evt) {
--- Serveur/main.cpp
+++ Serveur/main.cpp
@@ -15,7 +15,7 @@ int main()
     while(1)
     {
         server.update(-1, true);
-        std::cout << "oui222ddddddddddddddddd" << std::endl;
+        std::cout << "oui222" << std::endl;
     }
 
     return 0;
